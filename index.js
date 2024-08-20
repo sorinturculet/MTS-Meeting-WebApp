f43f5ea9844423e82b85b9df338237e87778e036
sorinturculet <81931461+sorinturculet@users.noreply.github.com>
2024-08-20 22:47:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCUbBJlSVxul-V4t42D95TLdq5jGSW95yg",
-    authDomain: "mts22-8adac.firebaseapp.com",
-    databaseURL: "https://mts22-8adac-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "mts22-8adac",
-    storageBucket: "mts22-8adac.appspot.com",
-    messagingSenderId: "625634894660",
-    appId: "1:625634894660:web:87641e46fcbea303f6ad11",
-    measurementId: "G-QZT4VY9YHP"
+    apiKey: "xxxxxxxxx",
+    authDomain: "xxxxxxx",
+    databaseURL: "xxxxxx",
+    projectId: "xxx",
+    storageBucket: "xxxxx",
+    messagingSenderId: "6xxxxxx",
+    appId: "xxxxxx",
+    measurementId: "xxxxxx"
   };
    // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
