61edd4577e54a43ab65ec8eceea79b07f161ed70
haipesintetic <81931461+haipesintetic@users.noreply.github.com>
2024-08-20 22:36:28 +0300

    final upload

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCUbBJlSVxul-V4t42D95TLdq5jGSW95yg",
+    authDomain: "mts22-8adac.firebaseapp.com",
+    databaseURL: "https://mts22-8adac-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "mts22-8adac",
+    storageBucket: "mts22-8adac.appspot.com",
+    messagingSenderId: "625634894660",
+    appId: "1:625634894660:web:87641e46fcbea303f6ad11",
+    measurementId: "G-QZT4VY9YHP"
+  };
--- index.js
+++ index.js
@@ -1,18 +1,42 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js";
 
-// Retrieve Firebase configuration from environment variables
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js";
+
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-};
+    apiKey: "AIzaSyCUbBJlSVxul-V4t42D95TLdq5jGSW95yg",
+    authDomain: "mts22-8adac.firebaseapp.com",
+    databaseURL: "https://mts22-8adac-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "mts22-8adac",
+    storageBucket: "mts22-8adac.appspot.com",
+    messagingSenderId: "625634894660",
+    appId: "1:625634894660:web:87641e46fcbea303f6ad11",
+    measurementId: "G-QZT4VY9YHP"
+  };
+   // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
-// Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
+  var numberCV;
+  function Ready(){
+    numberCV=document.getElementById('idParticipant').value;
+    console.log(numberCV);
+  }
+  function show(shown, hidden) {
+    
+    numberCV=document.getElementById('idParticipant').value;
+    if(numberCV>0 && numberCV<100)
+      {Ready();
+        InsertData();
+      document.getElementById(shown).style.display='block';
+      document.getElementById(hidden).style.display='none';
+      return false;
+      }
+    else alert("Hoțomane nu ai introdus un număr valid");
+  }
 
-// Export firebaseApp or use it as needed
+   function InsertData(){
+    firebase.database().ref('participant'+numberCV).once('value',
+    function(snapshot){
+        document.getElementById('namecv').value=snapshot.val().name;
+        }
+        );
+
+    }
